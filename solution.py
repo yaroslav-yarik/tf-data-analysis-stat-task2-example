8288e9677e8392343df6d78c741950f1c37dcb55
yaroslav-yarik <86802498+yaroslav-yarik@users.noreply.github.com>
2023-04-07 22:22:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,13 @@ from scipy.stats import norm
 chat_id = 561049466 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    #b = np.var(x) ** 0.5 * 2 + 0.098
-    #mu = (b + 0.098) / 2
-    #eps = p / 2 / (b - 0.098)
-    mu = np.mean(x)
-    eps = (mu - 0.098) * p
-    return (mu-eps, mu + eps)
+    left_bound = 0.098
+    max_x = max(x)
+    n = len(x)
+
+    g1 = (1 - p) ** (1 / n)
+    left = max_x
+    right = (max_x - left_bound) / g1 + left_bound
+    magic_size_add = (right - left) * 0.3
+
+    return (left, right + magic_size_add)
